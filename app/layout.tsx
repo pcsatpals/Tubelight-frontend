94cc3d7c1f53d18b6d2e6ea0a7054b7e9c16d3aa
pcsatpals <pcsatpals@gmail.com>
2025-12-10 22:51:54 +0530

    Created hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Figtree } from "next/font/google";
+import "../styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const figTreeSans = Figtree({
+  variable: "--font-figtree",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${figTreeSans.variable} ${figTreeSans.className} antialiased`}
       >
         {children}
       </body>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+   turbopack: {
+    // ...
+  },
+  reactStrictMode: false,
+  productionBrowserSourceMaps: true, // enable source maps in prod
+ webpack(config) {
+    config.devtool = 'source-map'; // emit source maps in development
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  experimental: {
+   
+    serverActions: {
+      bodySizeLimit: '50mb' // Increased from 2mb to 50mb
+    },
+  }
+}
 
 export default nextConfig;
